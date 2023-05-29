446874b5ecd4e91a9bcc27396535683174f2b4f9
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-29 17:21:06 +0500

    Remove book function added and localStorage feature also been added

--- script.js
+++ script.js
@@ -14,5 +14,40 @@ function addBooks(title,author){
     `;
     const prevHtml = document.getElementById('details').innerHTML;
     document.getElementById('details').innerHTML = html + prevHtml;
-   
+    updateBookData(collection);
+    var bookItems = document.querySelectorAll('.remove');
+    bookItems.forEach(function(bookItem) {
+      bookItem.addEventListener('click', removeBook);
+    });
 }
+
+function removeBook(event){
+    var parentDiv = event.target.parentElement;
+    var title = parentDiv.querySelector('.title').textContent;
+    var author = parentDiv.querySelector('.author').textContent;
+    
+    parentDiv.remove();
+    collectionFilter = collection.filter(function(book) {
+        return book.title !== title || book.author !== author;
+    });
+    updateBookData(collectionFilter);
+}
+Object.values(collection).forEach((field) => {
+    var html = `<div class="detailDiv"><p class="title">${field.title}</p>
+    <p class="author">${field.author}</p>
+    <button class="remove">Remove</button><hr></div>
+    `;
+    const prevHtml = document.getElementById('details').innerHTML;
+    document.getElementById('details').innerHTML = html + prevHtml;
+   
+    var bookItems = document.querySelectorAll('.remove');
+    bookItems.forEach(function(bookItem) {
+      bookItem.addEventListener('click', removeBook);
+    });
+  });
+
+function updateBookData(collectionData) {
+  
+  localStorage.setItem('booksCollection', JSON.stringify(collectionData));
+}
+
