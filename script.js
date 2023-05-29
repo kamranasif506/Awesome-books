0e640445a3fb282368959fb1fbc3d53245f6fdd4
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-29 17:28:43 +0500

    resolved linter  issues

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 const addBtn = document.getElementById('add');
 const collection = JSON.parse(localStorage.getItem('booksCollection')) || [];
-add.addEventListener('click',function(){
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    addBooks(title,author);
-})
-
-function addBooks(title,author){
-    collection.push({title:title,author:author});
-    var html = `<div class="detailDiv"><p class="title">${title}</p>
-    <p class="author">${author}</p>
-    <button class="remove">Remove</button><hr></div>
-    `;
-    const prevHtml = document.getElementById('details').innerHTML;
-    document.getElementById('details').innerHTML = html + prevHtml;
-    updateBookData(collection);
-    var bookItems = document.querySelectorAll('.remove');
-    bookItems.forEach(function(bookItem) {
-      bookItem.addEventListener('click', removeBook);
-    });
-}
-
-function removeBook(event){
-    var parentDiv = event.target.parentElement;
-    var title = parentDiv.querySelector('.title').textContent;
-    var author = parentDiv.querySelector('.author').textContent;
-    
-    parentDiv.remove();
-    collectionFilter = collection.filter(function(book) {
-        return book.title !== title || book.author !== author;
-    });
-    updateBookData(collectionFilter);
-}
-Object.values(collection).forEach((field) => {
-    var html = `<div class="detailDiv"><p class="title">${field.title}</p>
-    <p class="author">${field.author}</p>
-    <button class="remove">Remove</button><hr></div>
-    `;
-    const prevHtml = document.getElementById('details').innerHTML;
-    document.getElementById('details').innerHTML = html + prevHtml;
-   
-    var bookItems = document.querySelectorAll('.remove');
-    bookItems.forEach(function(bookItem) {
-      bookItem.addEventListener('click', removeBook);
-    });
-  });
 
 function updateBookData(collectionData) {
-  
   localStorage.setItem('booksCollection', JSON.stringify(collectionData));
 }
 
+function removeBook(event) {
+  const parentDiv = event.target.parentElement;
+  const title = parentDiv.querySelector('.title').textContent;
+  const author = parentDiv.querySelector('.author').textContent;
+
+  parentDiv.remove();
+  const collectionFilter = collection.filter(
+    (book) => book.title !== title || book.author !== author,
+  );
+  updateBookData(collectionFilter);
+}
+function addBooks(title, author) {
+  collection.push({ title, author });
+  const html = `<div class="detailDiv"><p class="title">${title}</p>
+      <p class="author">${author}</p>
+      <button class="remove">Remove</button><hr></div>
+      `;
+  const prevHtml = document.getElementById('details').innerHTML;
+  document.getElementById('details').innerHTML = html + prevHtml;
+  updateBookData(collection);
+  const bookItems = document.querySelectorAll('.remove');
+  bookItems.forEach((bookItem) => {
+    bookItem.addEventListener('click', removeBook);
+  });
+}
+addBtn.addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  addBooks(title, author);
+});
+
+Object.values(collection).forEach((field) => {
+  const html = `<div class="detailDiv"><p class="title">${field.title}</p>
+    <p class="author">${field.author}</p>
+    <button class="remove">Remove</button><hr></div>
+    `;
+  const prevHtml = document.getElementById('details').innerHTML;
+  document.getElementById('details').innerHTML = html + prevHtml;
+
+  const bookItems = document.querySelectorAll('.remove');
+  bookItems.forEach((bookItem) => {
+    bookItem.addEventListener('click', removeBook);
+  });
+});
