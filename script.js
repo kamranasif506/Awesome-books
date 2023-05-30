df8011a8f5cfdc51bcc1ca7abc00e378e45038ff
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-30 16:54:03 +0500

    convert all  the function into class BookCollection

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const addBtn = document.getElementById('add');
-const errorMsg = document.getElementById('error');
+class BookCollection {
+  constructor() {
+    this.title = document.getElementById('title');
+    this.author = document.getElementById('author');
+    this.addBtn = document.getElementById('add');
+    this.errorMsg = document.getElementById('error');
+    this.collection = JSON.parse(localStorage.getItem('booksCollection')) || [];
 
-errorMsg.style.display = 'none';
-const collection = JSON.parse(localStorage.getItem('booksCollection')) || [];
+    this.errorMsg.style.display = 'none';
 
-function updateBookData(collectionData) {
-  localStorage.setItem('booksCollection', JSON.stringify(collectionData));
-  title.value = '';
-  author.value = '';
-}
+    this.addBtn.addEventListener('click', () => {
+      if (this.title.value === '' || this.author.value === '') {
+        this.errorMsg.style.display = 'block';
+        this.errorMsg.style.color = 'red';
+      } else {
+        this.errorMsg.style.display = 'none';
+        this.addBook(this.title.value, this.author.value);
+      }
+    });
 
-function removeBook(event) {
-  const parentDiv = event.target.parentElement;
-  const title = parentDiv.querySelector('.title').textContent;
-  const author = parentDiv.querySelector('.author').textContent;
+    Object.values(this.collection).forEach((field) => {
+      const html = `<div class="detailDiv"><p class="title">${field.title}</p>
+        <p class="author">${field.author}</p>
+        <button class="remove">Remove</button><hr></div>
+        `;
+      const prevHtml = document.getElementById('details').innerHTML;
+      document.getElementById('details').innerHTML = html + prevHtml;
 
-  parentDiv.remove();
-  const collectionFilter = collection.filter(
-    (book) => book.title !== title || book.author !== author,
-  );
-  updateBookData(collectionFilter);
-}
-function addBooks(title, author) {
-  collection.push({ title, author });
-  const html = `<div class="detailDiv"><p class="title">${title}</p>
+      const bookItems = document.querySelectorAll('.remove');
+      bookItems.forEach((bookItem) => {
+        bookItem.addEventListener('click', this.removeBook.bind(this));
+      });
+    });
+  }
+
+  updateBookData(collectionData) {
+    localStorage.setItem('booksCollection', JSON.stringify(collectionData));
+    this.title.value = '';
+    this.author.value = '';
+  }
+
+  removeBook(event) {
+    const parentDiv = event.target.parentElement;
+    const title = parentDiv.querySelector('.title').textContent;
+    const author = parentDiv.querySelector('.author').textContent;
+
+    parentDiv.remove();
+    const collectionFilter = this.collection.filter(
+      (book) => book.title !== title || book.author !== author
+    );
+    this.collection = collectionFilter;
+    this.updateBookData(collectionFilter);
+  }
+
+  addBook(title, author) {
+    this.collection.push({ title, author });
+    const html = `<div class="detailDiv"><p class="title">${title}</p>
       <p class="author">${author}</p>
       <button class="remove">Remove</button><hr></div>
       `;
-  const prevHtml = document.getElementById('details').innerHTML;
-  document.getElementById('details').innerHTML = html + prevHtml;
-  updateBookData(collection);
-  const bookItems = document.querySelectorAll('.remove');
-  bookItems.forEach((bookItem) => {
-    bookItem.addEventListener('click', removeBook);
-  });
-}
-addBtn.addEventListener('click', () => {
-  if (title.value === '' || author.value === '') {
-    errorMsg.style.display = 'block';
-    errorMsg.style.color = 'red';
-  } else {
-    errorMsg.style.display = 'none';
-    addBooks(title.value, author.value);
+    const prevHtml = document.getElementById('details').innerHTML;
+    document.getElementById('details').innerHTML = html + prevHtml;
+    this.updateBookData(this.collection);
+
+    const bookItems = document.querySelectorAll('.remove');
+    bookItems.forEach((bookItem) => {
+      bookItem.addEventListener('click', this.removeBook.bind(this));
+    });
   }
-});
+}
 
-Object.values(collection).forEach((field) => {
-  const html = `<div class="detailDiv"><p class="title">${field.title}</p>
-    <p class="author">${field.author}</p>
-    <button class="remove">Remove</button><hr></div>
-    `;
-  const prevHtml = document.getElementById('details').innerHTML;
-  document.getElementById('details').innerHTML = html + prevHtml;
-
-  const bookItems = document.querySelectorAll('.remove');
-  bookItems.forEach((bookItem) => {
-    bookItem.addEventListener('click', removeBook);
-  });
-});
+// Create an instance of the BookCollection class
+const bookCollection = new BookCollection();
